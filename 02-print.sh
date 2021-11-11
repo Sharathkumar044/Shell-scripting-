237f9fcd29a8f9c8cad9957472054a44b700177d
Sharathkumar044 <67908876+Sharathkumar044@users.noreply.github.com>
2021-11-11 23:19:38 +0530

    normal

--- 02-print.sh
+++ 02-print.sh
@@ -14,7 +14,30 @@ echo my name is sharath , welcome to my redhat shell script
 
 echo "hello world/nWelcome"
 
+#colured output
+#syntax :echo -e "\e[COLmMessage"
+
+##colur     code
+# red         31
+# green       32
+#yellow       33
+# blue        34
+#magenta      35
+#cyan         36
 
 
+echo -e "\e[31mhisandya"
+##
+echo -e "\e[32mhi sandya"
+##
+echo -e "\e34m hi sandya"
+
+##colur always follows, when we enable colr and its pur reponsiblity to diable it as well,
+
+##0 col code is used to deable
+
+echo "\e[32m hisandya \e[0m"
+
+echo hisandya
 
 
