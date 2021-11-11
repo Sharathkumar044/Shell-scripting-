dcbd045ae4abe25a2d3ed4c607cf1730aa70fb33
Sharathkumar044 <67908876+Sharathkumar044@users.noreply.github.com>
2021-11-11 22:31:30 +0530

    changes

---
+++ 02-print.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+##tTo print some text on screen then we use "echo" command or printf command
+##we choose to go with echo command becaouse of its less sintaxing
+
+#syntax
+
+#echo message to print
+
+echo hello world
+echo my name is sharath , welcome to my redhat shell script
+
+#ESC sequenses
+
