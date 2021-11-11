c603036467ee0a4dd7fcf9335a2595be13c54452
Sharathkumar044 <67908876+Sharathkumar044@users.noreply.github.com>
2021-11-11 23:49:33 +0530

    normal

---
+++ 03-variables.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+##If we assign a name to set of data, then that is variable
+## SYNTAX :  VAR=DATA
+
+#number
+
+a=1100
+b=50020
+
+
+# in bash shell ans also by default there are no data types, Shell considers eveything as string
+
+#acess the data in shell using $ charecter prefiixing the variable name, or you can also acess varoable
+
+echo variable=$a
+echo value of a=$a
+echo value of b=$a
+
+c= today date is 11-11-21
+
+echo "hi,$[c]"
+
+
